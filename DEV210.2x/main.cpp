ef3910cae7af937aad1ed105e703a6b26edb5247
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-03-13 13:22:36 +0200

    Initial Commit

---
+++ DEV210.2x/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DEV210.2x
+//
+//  Created by Stefanos Mitropoulos on 13/03/2018.
+//  Copyright © 2018 Stefanos Mitropoulos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
